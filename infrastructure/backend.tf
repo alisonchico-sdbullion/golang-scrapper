1cd6eaaf21eb69881c93548198e5e343c3781443
Alison Chicoria <alisonchicoria@cloud.upwork.com>
2024-12-23 16:16:08 -0300

    Adjust backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "iac-tfstate-solo-exam-alison"
-#     key            = "ecs/api.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform_solo"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "iac-tfstate-solo-exam-alison"
+    key            = "ecs/api.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_solo"
+  }
+}
